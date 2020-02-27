d1f17b19eaa843c9124e994b3cd65f1a9f880b05
jack nutt <jack@infotorch.org>
2020-02-27 15:07:13 +0000

    base

---
+++ README.md
@@ -0,0 +1,19 @@
+# Data Analysis of Australian Government 2019 Sports Grant Awards
+
+Data analysis of sports funding awarded in 2019 prior to the election
+
+## Geocoding Results
+
+Results are geocoded against federal electoral boundaries SHP file
+
+Download from
+
+https://www.aec.gov.au/Electorates/gis/files/national-esri-fe2019.zip
+
+and unzip into `data`
+
+## Licenses
+
+This product incorporates data that is:
+
+© Commonwealth of Australia (Australian Electoral Commission) 2020
--- scrape_sportsdata.py
+++ scrape_sportsdata.py
@@ -10,23 +10,27 @@ from scrapy.settings import Settings
 
 OUTPUT_FILENAME = "data/grants.csv"
 
-locale.setlocale(locale.LC_ALL, 'en_AU.UTF8')
+locale.setlocale(locale.LC_ALL, "en_AU.UTF8")
+
 
 def parse_money(money=None):
     return locale.atoi(money.strip("$")) if money else None
+
 
 def parse_name(name=None):
     if not name:
         return None
 
     name = name.replace(os.linesep, "").replace("\r", "")
-    return re.sub('\s+', ' ', name).strip().title()
+    return re.sub(r"\s+", " ", name).strip().title()
+
 
 class SportsGrantItem(Item):
     club = Field()
     amount = Field()
     state = Field()
     rnd = Field()
+
 
 class SportsGrantItemLoader(ItemLoader):
     default_input_processor = MapCompose(str.strip)
@@ -36,6 +40,7 @@ class SportsGrantItemLoader(ItemLoader):
     amount_in = MapCompose(str.strip, parse_money)
     rnd_in = MapCompose(int)
 
+
 class SportsGrantSpider(Spider):
     name = "sportsgrant"
     allowed_domains = ["www.sportaus.gov.au"]
@@ -44,29 +49,34 @@ class SportsGrantSpider(Spider):
     ]
 
     def parse(self, response):
-        for sel in response.xpath('//table/tbody/tr'):
-            loader = SportsGrantItemLoader(SportsGrantItem(), selector=sel, response=response)
-            loader.add_xpath('club', 'td[1]//text()')
-            loader.add_xpath('amount', 'td[2]//text()')
-            loader.add_xpath('state', 'td[3]//text()')
-            loader.add_xpath('rnd', 'td[4]//text()')
+        for sel in response.xpath("//table/tbody/tr"):
+            loader = SportsGrantItemLoader(
+                SportsGrantItem(), selector=sel, response=response
+            )
+            loader.add_xpath("club", "td[1]//text()")
+            loader.add_xpath("amount", "td[2]//text()")
+            loader.add_xpath("state", "td[3]//text()")
+            loader.add_xpath("rnd", "td[4]//text()")
             yield loader.load_item()
 
-settings = Settings({
-    # 'ITEM_PIPELINES': {
-    #     '__main__.WriterPipeline': 100,
-    # },
-    'DOWNLOADER_MIDDLEWARES': {
-        # 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-    },
-    'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-    'FEED_FORMAT': 'csv',
-    'FEED_URI': OUTPUT_FILENAME
-})
+
+settings = Settings(
+    {
+        # 'ITEM_PIPELINES': {
+        #     '__main__.WriterPipeline': 100,
+        # },
+        "DOWNLOADER_MIDDLEWARES": {
+            # 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+        },
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+        "FEED_FORMAT": "csv",
+        "FEED_URI": OUTPUT_FILENAME,
+    }
+)
 
 if os.path.isfile(OUTPUT_FILENAME):
     os.remove(OUTPUT_FILENAME)
-    print("Removed file %s".format(OUTPUT_FILENAME))
+    print("Removed file {}".format(OUTPUT_FILENAME))
 
 process = CrawlerProcess(settings)
 
